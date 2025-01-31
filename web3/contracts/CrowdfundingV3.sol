4ff749ce4119d31f62378bc5cb3f38ebaac786ff
anarosu <anarosu766@gmail.com>
2025-01-31 02:20:20 +0000

    Implement Proxy Pattern, prevent reentrancy attacks, update unit tests

---
+++ web3/contracts/CrowdfundingV3.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "./libraries/CrowdfundingUtils.sol";
+import "./CommissionManager.sol";
+
+contract Crowdfunding is
+    Initializable,
+    OwnableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campaigns;
+    uint256 public numberOfCampaigns;
+    ICommissionManager public commissionManager;
+
+    event CampaignCreated(uint256 id, address owner, string title);
+    event CampaignEdited(uint256 id, string newTitle, string newDescription);
+    event DonationReceived(
+        uint256 campaignId,
+        address donator,
+        uint256 amount,
+        uint256 donatorShare
+    );
+    event RemainingAmountToRaise(uint256 campaignId, uint256 amount);
+    event Withdrawal(uint256 campaignId, address owner, uint256 amount);
+
+    modifier onlyOwnerOfCampaign(uint256 _id) {
+        require(msg.sender == campaigns[_id].owner, "Not the campaign owner.");
+        _;
+    }
+
+    function initialize(address payable _commissionManager) public initializer {
+        __Ownable_init(msg.sender);
+        __ReentrancyGuard_init();
+        commissionManager = ICommissionManager(_commissionManager);
+        numberOfCampaigns = 0;
+    }
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
+        require(_deadline > block.timestamp, "Deadline must be in the future.");
+
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+        emit CampaignCreated(numberOfCampaigns - 1, _owner, _title);
+        return numberOfCampaigns - 1;
+    }
+
+    function editCampaign(
+        uint256 _id,
+        string memory _newTitle,
+        string memory _newDescription
+    ) public onlyOwnerOfCampaign(_id) {
+        Campaign storage campaign = campaigns[_id];
+        require(
+            CrowdfundingUtils.isCampaignActive(campaign.deadline),
+            "Campaign is no longer active."
+        );
+        campaign.title = _newTitle;
+        campaign.description = _newDescription;
+
+        emit CampaignEdited(_id, _newTitle, _newDescription);
+    }
+
+    function donateToCampaign(uint256 _id) public payable nonReentrant {
+        Campaign storage campaign = campaigns[_id];
+        require(
+            CrowdfundingUtils.isCampaignActive(campaign.deadline),
+            "Campaign is no longer active."
+        );
+
+        uint256 amount = msg.value;
+        uint256 commission = CrowdfundingUtils.calculatePercentage(
+            amount,
+            commissionManager.getCommissionPercentage()
+        );
+        uint256 remainingDonationAmount = amount - commission;
+
+        campaign.amountCollected += remainingDonationAmount;
+        commissionManager.accumulate{value: commission}(commission);
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(remainingDonationAmount);
+
+        uint256 donatorShare = CrowdfundingUtils.calculateDonatorShare(
+            remainingDonationAmount,
+            campaign.amountCollected
+        );
+        emit DonationReceived(
+            _id,
+            msg.sender,
+            remainingDonationAmount,
+            donatorShare
+        );
+        emit RemainingAmountToRaise(
+            _id,
+            CrowdfundingUtils.calculateRemainingAmountToRaise(
+                campaign.target,
+                campaign.amountCollected
+            )
+        );
+    }
+
+    function withdrawFunds(
+        uint256 _id
+    ) public onlyOwnerOfCampaign(_id) nonReentrant {
+        Campaign storage campaign = campaigns[_id];
+        require(
+            CrowdfundingUtils.isCampaignActive(campaign.deadline) == false,
+            "Campaign is still active."
+        );
+
+        uint256 amount = campaign.amountCollected;
+        campaign.amountCollected = 0;
+
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        require(sent, "Withdrawal failed.");
+
+        emit Withdrawal(_id, campaign.owner, amount);
+    }
+
+    function getDonatorsOfCampaign(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            allCampaigns[i] = campaigns[i];
+        }
+        return allCampaigns;
+    }
+}
--- web3/test/Crowdfunding.js
+++ web3/test/Crowdfunding.js
@@ -1,146 +1,99 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("Crowdfunding", function () {
-  let crowdfunding;
-  let commissionManager;
-  let owner;
-  let donor1;
-  let donor2;
-  let otherAccount;
-  let campaignId;
+    async function deployContractsFixture() {
+        const [owner, donor1, donor2, otherAccount] = await ethers.getSigners();
+      
+        const CommissionManager = await ethers.getContractFactory("CommissionManager");
+        const commissionManager = await CommissionManager.deploy();
+        await commissionManager.waitForDeployment();
+      
+        const Crowdfunding = await ethers.getContractFactory("Crowdfunding");
+        const crowdfunding = await Crowdfunding.deploy();
+        await crowdfunding.waitForDeployment();
 
-  beforeEach(async () => {
-    // Deploy the CommissionManager contract
-    const CommissionManager = await ethers.getContractFactory("CommissionManager");
-    commissionManager = await CommissionManager.deploy();
-    await commissionManager.deployed();
+        await crowdfunding.initialize(commissionManager.target);
+      
+        await crowdfunding.createCampaign(
+          owner.address,
+          "Test Campaign",
+          "Description",
+          ethers.parseEther("10.0"), // Target: 10 ETH
+          Math.floor(Date.now() / 1000) + 24 * 60 * 60, // Deadline: 24 hours from now
+          "imageUrl"
+        );
+        const campaignId = 0;
+        return { crowdfunding, commissionManager, owner, donor1, donor2, otherAccount, campaignId };
+      }
 
-    // Deploy the Crowdfunding contract with the CommissionManager address
-    const Crowdfunding = await ethers.getContractFactory("Crowdfunding");
-    crowdfunding = await Crowdfunding.deploy(commissionManager.address);
-    await crowdfunding.deployed();
-
-    // Get the signers
-    [owner, donor1, donor2, otherAccount] = await ethers.getSigners();
-
-    // Create a campaign
-    const tx = await crowdfunding.createCampaign(
-      owner.address,
-      "Test Campaign",
-      "Description",
-      ethers.utils.parseEther("10.0"), // Target: 10 ETH
-      Math.floor(Date.now() / 1000) + 24 * 60 * 60, // Deadline: 24 hours from now
-      "imageUrl"
-    );
-    const receipt = await tx.wait();
-    const event = receipt.events.find((event) => event.event === "CampaignCreated");
-    campaignId = event.args.id;
-  });
-  
   describe("Create campaign", function () {
-    it("Should create a campaign", async () => {
+    it("Should create a campaign", async function () {
+      const { crowdfunding, campaignId, owner } = await loadFixture(deployContractsFixture);
+
       const campaign = await crowdfunding.campaigns(campaignId);
       expect(campaign.title).to.equal("Test Campaign");
-      expect(campaign.target).to.equal(ethers.utils.parseEther("10.0"));
+      expect(campaign.target).to.equal(ethers.parseEther("10.0"));
       expect(campaign.owner).to.equal(owner.address);
       expect(campaign.amountCollected).to.equal(0);
-      const currentTimestamp = ethers.BigNumber.from(Math.floor(Date.now() / 1000));
+
+      const currentTimestamp = Math.floor(Date.now() / 1000);
       expect(campaign.deadline).to.be.gt(currentTimestamp); // Deadline should be in the future
     });
 
-    it("Should not allow creation of a campaign with a deadline in the past", async () => {
+    it("Should not allow creation of a campaign with a deadline in the past", async function () {
+      const { crowdfunding, owner } = await loadFixture(deployContractsFixture);
+
       await expect(
         crowdfunding.createCampaign(
           owner.address,
           "Expired Campaign",
           "Expired",
-          ethers.utils.parseEther("10"),
+          ethers.parseEther("10"),
           Math.floor(Date.now() / 1000) - 60 * 60, // 1 hour in the past
           "expiredImage"
         )
-      ).to.be.revertedWith("The deadline should be a date in the future.");
+      ).to.be.revertedWith("Deadline must be in the future.");
     });
   });
 
   describe("Edit campaign", function () {
-    it("Should allow the owner to edit the campaign", async () => {
+    it("Should allow the owner to edit the campaign", async function () {
+      const { crowdfunding, owner, campaignId } = await loadFixture(deployContractsFixture);
+
       const newTitle = "Updated Campaign Title";
       const newDescription = "Updated Campaign Description";
-      const newTarget = ethers.utils.parseEther("20.0"); // New Target: 20 ETH
-      const newDeadline = Math.floor(Date.now() / 1000) + 6 * 60 * 60; // 6 hours from now
-      const newImage = "updatedImageUrl";
 
       await expect(
-        crowdfunding.connect(owner).editCampaign(
-          campaignId,
-          newTitle,
-          newDescription,
-          newTarget,
-          newDeadline,
-          newImage
-        )
-      ).to.emit(crowdfunding, "CampaignEdited")
+        crowdfunding.connect(owner).editCampaign(campaignId, newTitle, newDescription)
+      )
+        .to.emit(crowdfunding, "CampaignEdited")
         .withArgs(campaignId, newTitle, newDescription);
 
       const updatedCampaign = await crowdfunding.campaigns(campaignId);
       expect(updatedCampaign.title).to.equal(newTitle);
       expect(updatedCampaign.description).to.equal(newDescription);
-      expect(updatedCampaign.target).to.equal(newTarget);
-      expect(updatedCampaign.deadline).to.equal(newDeadline);
-      expect(updatedCampaign.image).to.equal(newImage);
     });
 
-    it("Should not allow non-owners to edit the campaign", async () => {
+    it("Should not allow non-owners to edit the campaign", async function () {
+      const { crowdfunding, otherAccount, campaignId } = await loadFixture(deployContractsFixture);
+
       const newTitle = "Unauthorized Edit";
       const newDescription = "This should fail";
 
       await expect(
-        crowdfunding.connect(otherAccount).editCampaign(
-          campaignId,
-          newTitle,
-          newDescription,
-          ethers.utils.parseEther("15.0"),
-          Math.floor(Date.now() / 1000) + 60 * 60, // 1 hour from now
-          "unauthorizedImage"
-        )
+        crowdfunding.connect(otherAccount).editCampaign(campaignId, newTitle, newDescription)
       ).to.be.revertedWith("Not the campaign owner.");
-    });
-  });
-
-  describe("Delete campaign", function () {
-    it("Should allow the owner to delete the campaign", async () => {
-      await expect(
-        crowdfunding.connect(owner).deleteCampaign(campaignId)
-      ).to.emit(crowdfunding, "CampaignDeleted")
-        .withArgs(campaignId, owner.address);
-
-      // Verify that the campaign is deleted
-      const deletedCampaign = await crowdfunding.campaigns(campaignId);
-      expect(deletedCampaign.owner).to.equal(ethers.constants.AddressZero); // Address zero indicates deletion
-      expect(deletedCampaign.title).to.equal(""); // Empty title
-    });
-
-    it("Should not allow non-owners to delete the campaign", async () => {
-      await expect(
-        crowdfunding.connect(otherAccount).deleteCampaign(campaignId)
-      ).to.be.revertedWith("Not the campaign owner.");
-    });
-
-    it("Should not allow the owner to delete a campaign with funds collected", async () => {
-      const donationAmount = ethers.utils.parseEther("1.0"); // 1 ETH
-      await crowdfunding.connect(donor1).donateToCampaign(campaignId, { value: donationAmount });
-
-      await expect(
-        crowdfunding.connect(owner).deleteCampaign(campaignId)
-      ).to.be.revertedWith("Cannot delete a campaign with collected funds.");
     });
   });
 
   describe("Donate to campaign", function () {
-    it("Should allow donations to the campaign", async () => {
-      const donationAmount = ethers.utils.parseEther("1.0"); // 1 ETH donation
-      const expectedAmount = donationAmount.mul(99).div(100); // 99% of the donation (after 1% commission)
+    it("Should allow donations to the campaign", async function () {
+      const { crowdfunding, donor1, campaignId } = await loadFixture(deployContractsFixture);
+
+      const donationAmount = ethers.parseEther("1.0"); // 1 ETH donation
+      const expectedAmount = donationAmount * 99n / 100n; // 99% of the donation (after 1% commission)
 
       await expect(
         crowdfunding.connect(donor1).donateToCampaign(campaignId, { value: donationAmount })
@@ -148,107 +101,88 @@ describe("Crowdfunding", function () {
         .to.emit(crowdfunding, "DonationReceived")
         .withArgs(campaignId, donor1.address, expectedAmount, 100)
         .and.to.emit(crowdfunding, "RemainingAmountToRaise")
-        .withArgs(campaignId, ethers.utils.parseEther("9.01"));
+        .withArgs(campaignId, ethers.parseEther("9.01"));
 
       const campaign = await crowdfunding.campaigns(campaignId);
       expect(campaign.amountCollected).to.equal(expectedAmount);
     });
 
     it("Should send commission to commission manager and interact correctly", async function () {
-      const donationAmount = ethers.utils.parseEther("1.0"); // 1 ETH donation
-      const expectedCommission = donationAmount.div(100); // 1% commission
-      const remainingDonationAmount = donationAmount.sub(expectedCommission);
-      const initialCommissionManagerBalance = await ethers.provider.getBalance(commissionManager.address);
-      await crowdfunding.connect(donor1).donateToCampaign(campaignId, { value: donationAmount })
-      const finalCommissionManagerBalance = await ethers.provider.getBalance(commissionManager.address);
-      expect(finalCommissionManagerBalance).to.equal(
-          initialCommissionManagerBalance.add(expectedCommission)
-      );
-      const campaign = await crowdfunding.campaigns(campaignId);
-      expect(campaign.amountCollected).to.equal(remainingDonationAmount);
-      const commissionManagerAddress = await crowdfunding.commissionManager();
-      expect(commissionManagerAddress).to.equal(commissionManager.address);
-    });
+      const { crowdfunding, commissionManager, donor1, campaignId } = await loadFixture(deployContractsFixture);
 
-    it("Should calculate the remaining amount to raise for the campaign", async () => {
-      const donationAmount = ethers.utils.parseEther("1"); // 1 ETH donation
+      const donationAmount = ethers.parseEther("1.0"); // 1 ETH donation
+      const expectedCommission = donationAmount / 100n; // 1% commission
+      const remainingDonationAmount = donationAmount - expectedCommission;
+
+      const initialCommissionManagerBalance = await ethers.provider.getBalance(commissionManager.target);
       await crowdfunding.connect(donor1).donateToCampaign(campaignId, { value: donationAmount });
 
+      const finalCommissionManagerBalance = await ethers.provider.getBalance(commissionManager.target);
+      expect(finalCommissionManagerBalance).to.equal(
+        initialCommissionManagerBalance + expectedCommission
+      );
+
       const campaign = await crowdfunding.campaigns(campaignId);
-      const remainingAmount = await crowdfunding.calculateRemainingAmountToRaise(
-        campaign.target,
-        campaign.amountCollected
-      );
-
-      expect(remainingAmount).to.equal(ethers.utils.parseEther("9.01")); // 10 ETH target - 0.99 ETH collected
+      expect(campaign.amountCollected).to.equal(remainingDonationAmount);
     });
 
-    it("Should calculate the donor's share correctly", async () => {
-      const donation1 = ethers.utils.parseEther("1");
-      const donation2 = ethers.utils.parseEther("2");
-      await crowdfunding.connect(donor1).donateToCampaign(campaignId, { value: donation1 });
-      await crowdfunding.connect(donor2).donateToCampaign(campaignId, { value: donation2 });
-      const campaign = await crowdfunding.campaigns(campaignId);
-      const donation1Commission = donation1.div(100); // 1% commission
-      const donation2Commission = donation2.div(100); // 1% commission
-      const remainingAmount1 = donation1.sub(donation1Commission); 
-      const remainingAmount2 = donation2.sub(donation2Commission); 
-      const totalAmountCollected = campaign.amountCollected;
-      const donor1Share = await crowdfunding.calculateDonatorShare(
-          remainingAmount1,
-          totalAmountCollected
-      );
-      const donor2Share = await crowdfunding.calculateDonatorShare(
-          remainingAmount2,
-          totalAmountCollected
-      );
-      
-      expect(donor1Share).to.equal(33);  
-      expect(donor2Share).to.equal(66);  
-    });
+    it("Should not allow donations if the campaign is inactive", async function () {
+        const { crowdfunding, donor1, owner } = await loadFixture(deployContractsFixture);
+        const tx = await crowdfunding.createCampaign(
+            owner.address,
+            "Expired Campaign",
+            "Expired",
+            ethers.parseEther("10"),
+            Math.floor(Date.now() / 1000) + 60 * 60, // 1 hour in the future
+            "expiredImage"
+        );
+        const receipt = await tx.wait();
+        const campaignCreatedEvent = receipt.logs.map((log) => {
+            try {
+                return crowdfunding.interface.parseLog(log);
+            } catch (e) {
+                return null;
+            }
+            })
+            .find((event) => event && event.name === "CampaignCreated");
 
-    it("Should not allow donations if the campaign is inactive", async () => {
-      const tx = await crowdfunding.createCampaign(
-        owner.address,
-        "Expired Campaign",
-        "Expired",
-        ethers.utils.parseEther("10"),
-        Math.floor(Date.now() / 1000) + 60 * 60, // 1 hour in the future
-        "expiredImage"
-      );
-      const receipt = await tx.wait();
-      const event = receipt.events.find((event) => event.event === "CampaignCreated");
-      const inactiveCampaignId = event.args.id;
+        const inactiveCampaignId = campaignCreatedEvent.args.id;
+        // Increase time to make the campaign inactive
+        const currentBlockTimestamp = (await ethers.provider.getBlock("latest")).timestamp;
+        const futureTimestamp = currentBlockTimestamp + 60 * 60 * 2; // 2 hours in the future
+        await ethers.provider.send("evm_setNextBlockTimestamp", [futureTimestamp]);
+        await ethers.provider.send("evm_mine", []);
 
-      const currentBlockTimestamp = (await ethers.provider.getBlock("latest")).timestamp;
-      const futureTimestamp = currentBlockTimestamp + 60 * 60 * 2; // 2 hours in the future
-      await ethers.provider.send("evm_setNextBlockTimestamp", [futureTimestamp]);
-      await ethers.provider.send("evm_mine", []);
-
-      await expect(
-        crowdfunding.connect(donor1).donateToCampaign(inactiveCampaignId, { value: ethers.utils.parseEther("1") })
-      ).to.be.revertedWith("Campaign is no longer active.");
-    });
+        await expect(
+            crowdfunding.connect(donor1).donateToCampaign(inactiveCampaignId, { value: ethers.parseEther("1") })
+        ).to.be.revertedWith("Campaign is no longer active.");
+        });
   });
 
   describe("Withdraw funds", function () {
-    it("Should not allow non-owners to withdraw funds", async () => {
+    it("Should not allow non-owners of the campaign to withdraw funds", async function () {
+      const { crowdfunding, otherAccount, campaignId } = await loadFixture(deployContractsFixture);
+
       await expect(
         crowdfunding.connect(otherAccount).withdrawFunds(campaignId)
       ).to.be.revertedWith("Not the campaign owner.");
     });
 
-    it("Should not allow the owner to withdraw funds before the campaign ends", async () => {
-      const donationAmount = ethers.utils.parseEther("1.0");
+    it("Should not allow the owner to withdraw funds before the campaign ends", async function () {
+      const { crowdfunding, donor1, owner, campaignId } = await loadFixture(deployContractsFixture);
+
+      const donationAmount = ethers.parseEther("1.0");
       await crowdfunding.connect(donor1).donateToCampaign(campaignId, { value: donationAmount });
 
       await expect(
         crowdfunding.connect(owner).withdrawFunds(campaignId)
-      ).to.be.revertedWith("Funds can only be withdrawn if the campaign deadline has passed.");
+      ).to.be.revertedWith("Campaign is still active.");
     });
 
-    it("Should allow the owner to withdraw funds after the campaign ends", async () => {
-      const donationAmount = ethers.utils.parseEther("1.0");
+    it("Should allow the owner to withdraw funds after the campaign ends", async function () {
+      const { crowdfunding, donor1, owner, campaignId } = await loadFixture(deployContractsFixture);
+
+      const donationAmount = ethers.parseEther("1.0");
       await crowdfunding.connect(donor1).donateToCampaign(campaignId, { value: donationAmount });
 
       await ethers.provider.send("evm_increaseTime", [24 * 60 * 60]); // Increase time by 24 hours
@@ -260,6 +194,30 @@ describe("Crowdfunding", function () {
 
       const updatedCampaign = await crowdfunding.campaigns(campaignId);
       expect(updatedCampaign.amountCollected).to.equal(0); // Funds withdrawn
+    });
+  });
+
+  describe("View functions", function () {
+    it("Should return the donators and donations of a campaign", async function () {
+        const { crowdfunding, donor1, donor2, campaignId } = await loadFixture(deployContractsFixture);
+    
+        const donationAmount = ethers.parseEther("1.0");
+        await crowdfunding.connect(donor1).donateToCampaign(campaignId, { value: donationAmount });
+        await crowdfunding.connect(donor2).donateToCampaign(campaignId, { value: donationAmount });
+        const remainingDonationAmount = donationAmount * 99n / 100n; 
+    
+        const [donators, donations] = await crowdfunding.getDonatorsOfCampaign(campaignId);
+        expect(donators).to.deep.equal([donor1.address, donor2.address]);
+        expect(donations).to.deep.equal([remainingDonationAmount, remainingDonationAmount]);
+    });
+
+    it("Should return the campaigns", async function () {
+        const { crowdfunding, campaignId, owner } = await loadFixture(deployContractsFixture);
+        const campaigns = await crowdfunding.getCampaigns();
+        expect(campaigns).to.have.lengthOf(1);
+        const campaign = campaigns[0];
+        expect(campaign.owner).to.equal(owner.address); 
+        expect(campaign.title).to.equal("Test Campaign");
     });
   });
 });
